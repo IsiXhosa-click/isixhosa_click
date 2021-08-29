82c07dffa336f554e092a5f046a10aed0b32ead8
Restioson <restiosondev@gmail.com>
2021-08-29 14:54:55 +0200

    beginnings of auth features

--- src/main.rs
+++ src/main.rs
@@ -304,8 +304,7 @@ async fn main() {
         .recover(handle_auth_error)
         .or(auth::with_any_auth(db).map(|auth, _db| {
             warp::reply::with_status(NotFound { auth }, StatusCode::NOT_FOUND).into_response()
-        }))
-    ;
+        }));
 
     log::info!("Visit https://127.0.0.1:{}/", cfg.https_port);
 
