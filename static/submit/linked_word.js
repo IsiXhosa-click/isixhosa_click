8a34830c8ff1467d6b52f684f4ed803f88de324d
Restioson <restiosondev@gmail.com>
2021-09-14 11:02:19 +0200

    update version of js

--- static/submit/linked_word.js
+++ static/submit/linked_word.js
@@ -1,4 +1,4 @@
-import { LiveSearch, formatResult } from "/live_search.js";
+import { LiveSearch, formatResult } from "/live_search.js?v=1";
 import { addFormData } from "/submit/util.js";
 
 let current_linked_word_id = 0;
