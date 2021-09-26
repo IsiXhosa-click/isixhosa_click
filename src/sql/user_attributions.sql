f628294d96cd0f7a034e18f8316cd9b649b9d870
Restioson <restiosondev@gmail.com>
2021-09-26 17:56:30 +0200

    fix

--- src/sql/user_attributions.sql
+++ src/sql/user_attributions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS user_attributions (
-    word_id  INTEGER PRIMARY KEY REFERENCES words(word_id) ON DELETE CASCADE,
+    word_id  INTEGER NOT NULL REFERENCES words(word_id) ON DELETE CASCADE,
     user_id  INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
     UNIQUE(word_id, user_id) ON CONFLICT IGNORE
 );
