7eb1436e3e4320abf3d18e80fe69b2ea4132d2b0
Restioson <restiosondev@gmail.com>
2024-05-31 23:48:13 +0200

    fix: service worker caching (again)

--- server/build.rs
+++ server/build.rs
@@ -1,12 +1,12 @@
 use std::process::Command;
 
-fn get_last_changed(path: &str) -> String {
+fn get_last_changed(path: &str, name_pat: &str) -> String {
     let output = Command::new("bash")
         // Taken from a comment in https://stackoverflow.com/a/7448828
         .args([
             "-c",
             &format!(
-                "find {path} -type f -print0 | xargs -0 stat --format '%Y' |\
+                "find {path} -type f {name_pat} -print0 | xargs -0 stat --format '%Y' |\
                  sort -nr | cut -d: -f2- | head -1"
             ),
         ])
@@ -18,12 +18,19 @@ fn get_last_changed(path: &str) -> String {
 
 fn main() {
     println!("cargo:rerun-if-changed=static/");
+
+    let static_exts = ["png", "svg", "woff2", "ico"]
+        .map(|ext| format!("-name '*.{ext}'"))
+        .join(" -o ");
+    let static_pat = format!("\\( {static_exts} \\)");
+
     println!(
         "cargo:rustc-env=STATIC_LAST_CHANGED={}",
-        get_last_changed("static").trim()
+        get_last_changed("static", &format!("! {static_pat}")).trim()
     );
+
     println!(
         "cargo:rustc-env=STATIC_BIN_FILES_LAST_CHANGED={}",
-        get_last_changed("static/**/*.{png,svg,woff2,ico}").trim()
+        get_last_changed("static", &static_pat).trim()
     );
 }
